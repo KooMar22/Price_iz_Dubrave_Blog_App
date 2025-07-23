321ed04aa8bc0ed64e5f318c4a59a7fb23206627
KooMar <djhracak@gmail.com>
2025-07-23 17:00:44 +0200

    migrating prisma indexing to neon

---
+++ server/prisma/migrations/20250723150006_add_post_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "comments_post_id_idx" ON "comments"("post_id");
+
+-- CreateIndex
+CREATE INDEX "comments_user_id_idx" ON "comments"("user_id");
+
+-- CreateIndex
+CREATE INDEX "comments_created_at_idx" ON "comments"("created_at");
